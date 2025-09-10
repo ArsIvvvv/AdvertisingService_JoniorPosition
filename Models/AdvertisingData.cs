2115bc5aa8dddb4a6825138308b2143598160626
Ivlev Arseniy <ivlev@pamparam.com>
2025-09-11 00:54:51 +0300

    Добавил Models

---
+++ Models/AdvertisingData.cs
@@ -0,0 +1,7 @@
+﻿namespace AdvertisingService.Models
+{
+    public class AdvertisingData
+    {
+        public List<Platform> Platforms { get; set; } = new List<Platform>();
+    }
+}
---
+++ Models/Platform.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvertisingService.Models
+{
+    public class Platform
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<string> Locations { get; set; } = new List<string>();
+    }
+}
